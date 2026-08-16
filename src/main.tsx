574c27aa2bbeecb473b58193a7bc70335d736c46
flanhenrique <walflanribeiro@gmail.com>
2026-08-16 18:58:55 -0400

    feat: route admin workspace at /admin/orcamentos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import AdminQuotes from './quotes/AdminQuotes'
 import './styles.css'
+
+const pathname = window.location.pathname.replace(/\/+$/, '') || '/'
+const isAdminRoute = pathname === '/admin/orcamentos' || window.location.hash === '#admin/orcamentos'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdminRoute ? <AdminQuotes /> : <App />}
   </StrictMode>,
 )
