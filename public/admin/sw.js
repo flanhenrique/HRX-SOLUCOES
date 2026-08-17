7ee34e707baa2161fba556c83d8f9feb28d98c2f
flanhenrique <walflanribeiro@gmail.com>
2026-08-16 20:26:15 -0400

    fix: refresh admin PWA shell cache

--- public/admin/sw.js
+++ public/admin/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hrx-admin-v3'
+const CACHE_NAME = 'hrx-admin-v4'
 const APP_SHELL = ['/admin/orcamentos', '/admin/manifest.webmanifest', '/admin/hrx-admin-icon.svg']
 
 self.addEventListener('install', (event) => {
