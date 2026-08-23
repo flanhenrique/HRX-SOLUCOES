c9a21c06a0fbba2c9307c63a81332c115702e3bb
flanhenrique <walflanribeiro@gmail.com>
2026-08-23 15:44:33 -0400

    feat(admin): enable personalization and responsive hardening

--- src/quotes/AdminApp.tsx
+++ src/quotes/AdminApp.tsx
@@ -2,12 +2,14 @@ import AdminQuotes from './AdminQuotes'
 import AdminSuspensionsPage from './AdminSuspensionsPage'
 import AdminFiscalPage from './AdminFiscalPage'
 import AdminExperienceLayer from './AdminExperienceLayer'
+import AdminPersonalizationBridge from './AdminPersonalizationBridge'
 import SuspendedQuoteGuard from './SuspendedQuoteGuard'
 import './admin-page-system.css'
 import './admin-feedback.css'
 import './admin-interactions.css'
 import './admin-executive-intelligence.css'
 import './admin-liquid-glass.css'
+import './admin-responsive-hardening.css'
 
 export default function AdminApp() {
   return <>
@@ -15,6 +17,7 @@ export default function AdminApp() {
     <AdminSuspensionsPage />
     <AdminFiscalPage />
     <AdminExperienceLayer />
+    <AdminPersonalizationBridge />
     <SuspendedQuoteGuard />
   </>
 }
