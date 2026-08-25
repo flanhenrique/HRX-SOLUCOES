36968ed52a26634cc72c915e131ce19237a9eddf
flanhenrique <walflanribeiro@gmail.com>
2026-08-25 14:16:41 -0400

    fix(pwa): carregar ajuste final de viewport do iPhone

--- src/quotes/AdminApp.tsx
+++ src/quotes/AdminApp.tsx
@@ -10,6 +10,7 @@ import './admin-unified-shell.css'
 import './admin-unified-chrome.css'
 import './admin-mobile-usability-fixes.css'
 import './admin-mobile-floating-dock-fix.css'
+import './admin-ios-viewport-dock-fix.css'
 
 export default function AdminApp() {
   return <AdminUnifiedRoot />
